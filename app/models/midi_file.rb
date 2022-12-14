2d581121d0b4d5bbf3031593498137e5c268260b
taishi-sama <alexsandr.2002.m@gmail.com>
2022-12-14 15:49:03 +0300

    Update to use Active Storage

--- app/models/midi_file.rb
+++ app/models/midi_file.rb
@@ -3,4 +3,8 @@ class MidiFile < ApplicationRecord
     has_many :accesses, dependent: :destroy
     has_many :users, through: :accesses
     has_many :links, dependent: :destroy
+
+    has_one_attached :midi
+    has_one_attached :fluidsynth
+    
 end
--- db/migrate/20221129174242_create_midi_files.rb
+++ db/migrate/20221129174242_create_midi_files.rb
@@ -1,11 +1,9 @@
 class CreateMidiFiles < ActiveRecord::Migration[7.0]
   def change
     create_table :midi_files do |t|
-      t.string :filename
       t.string :song_name
       t.string :author
       t.boolean :public
-
       t.timestamps
     end
   end
