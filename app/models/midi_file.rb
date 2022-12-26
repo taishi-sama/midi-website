4bf99311e5da367acc6b60583a091dbb6364a6f9
taishi-sama <73967202+taishi-sama@users.noreply.github.com>
2022-12-26 07:48:56 +0000

    Limit max file size

--- app/models/midi_file.rb
+++ app/models/midi_file.rb
@@ -1,6 +1,7 @@
 class MidiFile < ApplicationRecord
-    validates :song_name, presence: true
+    validates :song_name, :midi, presence: true
     validates :public, exclusion: [nil]
+    validates :validate_midi_max_size
     belongs_to :user
     has_many :accesses, dependent: :destroy
     has_many :accessed_users, through: :accesses, class_name: :users
@@ -8,5 +9,11 @@ class MidiFile < ApplicationRecord
 
     has_one_attached :midi
     has_one_attached :fluidsynth
-    
+    private
+    def validate_midi_max_size
+        if midi.byte_size > 500 * 1024
+            errors.add(:midi, 'File can\'t be larger 500 kilobytes')
+        end
+    end
+
 end
