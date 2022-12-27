d528b5c3e65da39387ab6fa9ca525b578f2662bc
taishi-sama <alexsandr.2002.m@gmail.com>
2022-12-27 18:39:32 +0300

    Validation for player

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -2,5 +2,8 @@ class PlayerController < ApplicationController
   def play
     #TODO: Сделать валидацию на возможность доступа к файлу
     @current_song = MidiFile.find(params[:id]);
+    unless @current_song.available_for?(session[:user_id])
+      redirect_to root_path
+    end
   end
 end
